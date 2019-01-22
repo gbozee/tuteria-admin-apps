209e2d4e92583265d323688f9ed59f0658a27b13
Beesama <gbozee@gmail.com>
2019-01-22 10:14:04 +0100

    minor changes to request details

--- src/pages/RequestDetailsPage.js
+++ src/pages/RequestDetailsPage.js
@@ -1,9 +1,9 @@
 /** @jsx jsx */
-import React, { Component } from 'react';
-import { css, jsx } from '@emotion/core';
-import { Text, Flex, Box, Button as RButton } from '@rebass/emotion';
-import { Button, DialogButton } from 'tuteria-shared/lib/shared/primitives';
-import { Input } from 'tuteria-shared/lib/shared/LoginPage';
+import React, { Component } from "react";
+import { css, jsx } from "@emotion/core";
+import { Text, Flex, Box, Button as RButton } from "@rebass/emotion";
+import { Button, DialogButton } from "tuteria-shared/lib/shared/primitives";
+import { Input } from "tuteria-shared/lib/shared/LoginPage";
 import {
   DetailItem,
   DetailHeader,
@@ -12,29 +12,29 @@ import {
   RequestListItem,
   SectionListPage,
   BookingDetailHeader,
-  getDate,
-} from 'tuteria-shared/lib/shared/reusables';
-import { Select } from './PVerificationDetailPage';
+  getDate
+} from "tuteria-shared/lib/shared/reusables";
+import { Select } from "./PVerificationDetailPage";
 
 const options = [
-  { label: 'To be booked', value: 'To be booked' },
-  { label: 'Completed', value: 'Completed' },
-  { label: 'Cold', value: 'Cold' },
+  { label: "To be booked", value: "To be booked" },
+  { label: "Completed", value: "Completed" },
+  { label: "Cold", value: "Cold" }
 ];
 
 const days = [
-  { label: 'Monday', value: 'Monday' },
-  { label: 'Tuesday', value: 'Tuesday' },
-  { label: 'Wednesday', value: 'Wednesday' },
-  { label: 'Thursday', value: 'Thursday' },
-  { label: 'Friday', value: 'Friday' },
-  { label: 'Saturday', value: 'Saturday' },
-  { label: 'Sunday', value: 'Sunday' },
+  { label: "Monday", value: "Monday" },
+  { label: "Tuesday", value: "Tuesday" },
+  { label: "Wednesday", value: "Wednesday" },
+  { label: "Thursday", value: "Thursday" },
+  { label: "Friday", value: "Friday" },
+  { label: "Saturday", value: "Saturday" },
+  { label: "Sunday", value: "Sunday" }
 ];
 
 const duration = [
-  { label: '10:00am - 2:00pm', value: '10:00am - 2:00pm' },
-  { label: '2:00pm - 4:00pm', value: '2:00pm - 4:00pm' },
+  { label: "10:00am - 2:00pm", value: "10:00am - 2:00pm" },
+  { label: "2:00pm - 4:00pm", value: "2:00pm - 4:00pm" }
 ];
 
 const RemarkForm = ({ onSubmit }) => (
@@ -54,15 +54,15 @@ const RemarkForm = ({ onSubmit }) => (
 
 class BookingForm extends Component {
   state = {
-    fields: this.props.fields || {},
+    fields: this.props.fields || {}
   };
   onChange = (field, callback) => e => {
     let result = callback(e);
     this.setState(({ fields }) => ({
       fields: {
         ...fields,
-        [field]: result,
-      },
+        [field]: result
+      }
     }));
   };
   onSubmit = () => {
@@ -90,7 +90,7 @@ class BookingForm extends Component {
                 label="Start date"
                 placeholder="10/1/2019"
                 value={fields.start_date}
-                onChange={this.onChange('start_date', e => e.target.value)}
+                onChange={this.onChange("start_date", e => e.target.value)}
               />
             </Box>
             <Box
@@ -103,7 +103,7 @@ class BookingForm extends Component {
                 label="Start time"
                 placeholder="10:00am"
                 value={fields.start_time}
-                onChange={this.onChange('start_time', e => e.target.value)}
+                onChange={this.onChange("start_time", e => e.target.value)}
               />
             </Box>
           </Flex>
@@ -123,7 +123,7 @@ class BookingForm extends Component {
                 label="Duration"
                 value={fields.duration}
                 placeholder="Select lesson duration"
-                onChange={this.onChange('duration', e => e.target.value)}
+                onChange={this.onChange("duration", e => e.target.value)}
               />
             </Box>
             <Box
@@ -136,7 +136,7 @@ class BookingForm extends Component {
                 value={fields.days}
                 label="Days of week"
                 placeholder="Select lesson days"
-                onChange={this.onChange('days', e => e.target.value)}
+                onChange={this.onChange("days", e => e.target.value)}
               />
             </Box>
           </Flex>
@@ -155,7 +155,7 @@ class BookingForm extends Component {
                 label="Hours per day"
                 placeholder="2"
                 value={fields.hours}
-                onChange={this.onChange('hours', e => e.target.value)}
+                onChange={this.onChange("hours", e => e.target.value)}
               />
             </Box>
           </Flex>
@@ -176,102 +176,105 @@ class BookingForm extends Component {
 
 export class RequestDetailPage extends Component {
   state = {
-    status: 'To be booked',
+    status: "To be booked",
     data: {
       request: {
+        full_name: "Tioluwani Kolawole",
+        email: "kolawole.tioluwani@gmail.com",
+        phone: "08078657912",
         user: {
-          full_name: 'Tioluwani Kolawole',
-          email: 'kolawole.tioluwani@gmail.com',
-          phone: '08078657912',
+          full_name: "Tioluwani Kolawole",
+          email: "kolawole.tioluwani@gmail.com",
+          phone: "08078657912"
         },
-        request_id: '23009',
+        request_id: "23009",
         no_of_hours: 7,
         per_hour_rate: 2500,
         budget: 35000,
-        slug: 'ABSCDEFG',
-        start_date: '2018-10-10 9:20:33',
-        start_time: '9:20:33',
+        slug: "ABSCDEFG",
+        start_date: "2018-10-10 9:20:33",
+        start_time: "9:20:33",
         no_of_students: 3,
-        curriculum: 'British',
-        duration: '3pm - 5pm',
-        classes: ['JSS1', 'JSS2', 'SS1'],
-        days: ['Monday', 'Wednesday', 'Friday', 'Saturday'],
-        subjects: ['Mathematics', 'English Language', 'Biology', 'Chemistry'],
+        curriculum: "British",
+        duration: "3pm - 5pm",
+        classes: ["JSS1", "JSS2", "SS1"],
+        days: ["Monday", "Wednesday", "Friday", "Saturday"],
+        subjects: ["Mathematics", "English Language", "Biology", "Chemistry"],
         expectation:
-          'Lorem Khaled Ipsum is a major key to success. I’m giving you cloth talk, cloth. Special cloth alert, cut from a special cloth. The first of the month is coming, we have to get money, we have no choice.',
+          "Lorem Khaled Ipsum is a major key to success. I’m giving you cloth talk, cloth. Special cloth alert, cut from a special cloth. The first of the month is coming, we have to get money, we have no choice."
       },
       location: {
-        area: 'lbs',
-        state: 'Lagos',
-        address: '37 Alara street, Onike ',
-        vicinity: 'Yaba',
+        area: "lbs",
+        state: "Lagos",
+        address: "37 Alara street, Onike ",
+        vicinity: "Yaba"
       },
       tutors: [
         {
           profile_pic:
-            'http://res.cloudinary.com/tuteria/image/upload/v1546853059/vc4x8ljjgaollupn25x0.jpg',
-          phone_no: '+2348069269483',
-          full_name: 'Blessing Avugara',
+            "http://res.cloudinary.com/tuteria/image/upload/v1546853059/vc4x8ljjgaollupn25x0.jpg",
+          phone_no: "+2348069269483",
+          full_name: "Blessing Avugara",
           verified: true,
           amount_per_hour: 2500,
-          years_of_experience: 'Between 3 to 5 years',
-          email: 'blessingozioma123@gmail.com',
+          years_of_experience: "Between 3 to 5 years",
+          email: "blessingozioma123@gmail.com",
           potential_subjects: [
-            'Verbal Reasoning',
-            'Fine Art',
-            'Quantitative Reasoning',
-            'Basic Sciences',
-            'Basic Mathematics',
-            'English Language',
-            'Computer Science',
-            'Social Studies',
-            'Economics',
-            'Commerce',
-          ],
+            "Verbal Reasoning",
+            "Fine Art",
+            "Quantitative Reasoning",
+            "Basic Sciences",
+            "Basic Mathematics",
+            "English Language",
+            "Computer Science",
+            "Social Studies",
+            "Economics",
+            "Commerce"
+          ]
         },
         {
           profile_pic:
-            'http://res.cloudinary.com/tuteria/image/upload/v1546853059/vc4x8ljjgaollupn25x0.jpg',
-          phone_no: '+2348069269483',
-          full_name: 'Blessing Avugara',
+            "http://res.cloudinary.com/tuteria/image/upload/v1546853059/vc4x8ljjgaollupn25x0.jpg",
+          phone_no: "+2348069269483",
+          full_name: "Blessing Avugara",
           verified: true,
           amount_per_hour: 2500,
-          years_of_experience: 'Between 3 to 5 years',
-          email: 'blessingozioma123@gmail.com',
+          years_of_experience: "Between 3 to 5 years",
+          email: "blessingozioma123@gmail.com",
           potential_subjects: [
-            'Verbal Reasoning',
-            'Fine Art',
-            'Quantitative Reasoning',
-            'Basic Sciences',
-            'Basic Mathematics',
-            'English Language',
-            'Computer Science',
-            'Social Studies',
-            'Economics',
-            'Commerce',
-          ],
-        },
-      ],
-    },
+            "Verbal Reasoning",
+            "Fine Art",
+            "Quantitative Reasoning",
+            "Basic Sciences",
+            "Basic Mathematics",
+            "English Language",
+            "Computer Science",
+            "Social Studies",
+            "Economics",
+            "Commerce"
+          ]
+        }
+      ]
+    }
   };
   onStatusChange = e => {
     this.setState({
-      status: e.target.value,
+      status: e.target.value
     });
   };
   filteredResults = () => {
     return [
       {
-        slug: 'ABCDESDDESS',
-        full_name: 'Shola Ameobi',
-        email: 'james@example.com',
-        phone_no: '08033002232',
-        skill: 'IELTS',
-        tutor: 'Chidiebere',
-        status: 'pending',
-        created: '2018-10-12 14:10:33',
-        modified: '2018-10-12 14:10:33',
-      },
+        slug: "ABCDESDDESS",
+        full_name: "Shola Ameobi",
+        email: "james@example.com",
+        phone_no: "08033002232",
+        skill: "IELTS",
+        tutor: "Chidiebere",
+        status: "pending",
+        created: "2018-10-12 14:10:33",
+        modified: "2018-10-12 14:10:33"
+      }
     ];
   };
   render() {
@@ -284,24 +287,24 @@ export class RequestDetailPage extends Component {
             detail={[
               request.user.email,
               request.user.full_name,
-              request.user.phone,
+              request.user.phone
             ]}
           >
             <Flex flexDirection="column">
               <Text pb={2}>
-                <strong>Slug:</strong> {request.slug}{' '}
+                <strong>Slug:</strong> {request.slug}{" "}
               </Text>
               <Text pb={2}>
-                <strong>Request ID:</strong> {request.request_id}{' '}
+                <strong>Request ID:</strong> {request.request_id}{" "}
               </Text>
               <Text pb={2}>
-                <strong>Budget:</strong> {request.budget}{' '}
+                <strong>Budget:</strong> {request.budget}{" "}
               </Text>
               <Text pb={2}>
-                <strong>Per hour rate:</strong> {request.per_hour_rate}{' '}
+                <strong>Per hour rate:</strong> {request.per_hour_rate}{" "}
               </Text>
               <Text>
-                <strong>Number of hours:</strong> {request.no_of_hours}{' '}
+                <strong>Number of hours:</strong> {request.no_of_hours}{" "}
               </Text>
             </Flex>
           </TutorDetailHeader>
@@ -358,7 +361,7 @@ export class RequestDetailPage extends Component {
               css={css`
                 border-bottom: 1px solid #e8e8e8;
               `}
-              mb={'16px'}
+              mb={"16px"}
             >
               <TutorDetailHeader
                 image={tutor.profile_pic}
@@ -366,7 +369,7 @@ export class RequestDetailPage extends Component {
                   tutor.years_of_experience,
                   tutor.full_name,
                   tutor.email,
-                  tutor.phone_no,
+                  tutor.phone_no
                 ]}
               >
                 <Text>N{tutor.amount_per_hour}</Text>
@@ -399,7 +402,7 @@ export class RequestDetailPage extends Component {
           </Flex>
         </Box>
         <div>
-          {this.state.status.toLowerCase() === 'to be booked' && (
+          {this.state.status.toLowerCase() === "to be booked" && (
             <DialogButton
               hideFooter={true}
               heading="Create Booking"
@@ -409,7 +412,7 @@ export class RequestDetailPage extends Component {
                     start_date: getDate(request.start_date),
                     start_time: request.start_time,
                     duration: request.duration,
-                    hours: request.no_of_hours,
+                    hours: request.no_of_hours
                   }}
                 />
               }
@@ -418,7 +421,7 @@ export class RequestDetailPage extends Component {
             </DialogButton>
             // <Button>Create Booking</Button>
           )}
-          {this.state.status.toLowerCase() === 'cold' && (
+          {this.state.status.toLowerCase() === "cold" && (
             <Button>Add to mailing list</Button>
           )}
         </div>
@@ -435,7 +438,7 @@ export class RequestDetailPage extends Component {
               start_date: getDate(request.start_date),
               start_time: request.start_time,
               duration: request.duration,
-              hours: request.no_of_hours,
+              hours: request.no_of_hours
             }}
           />
         </Box>
